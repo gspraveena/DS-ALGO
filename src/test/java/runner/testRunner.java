313507131de344b938b28327fa377fcf520b5dec
Reka <rekaharisri@gmail.com>
2023-02-24 16:12:09 -0600

    Y

--- src/test/java/runner/testRunner.java
+++ src/test/java/runner/testRunner.java
@@ -2,8 +2,8 @@ package runner;
 import org.junit.runner.RunWith;
 import org.testng.annotations.DataProvider;
 import io.cucumber.junit.Cucumber;
-//import io.cucumber.junit.CucumberOptions;
-import io.cucumber.testng.CucumberOptions;
+import io.cucumber.junit.CucumberOptions;  //***************  junit
+//import io.cucumber.testng.CucumberOptions;   //***************  testng
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 /*DONE DATA DRIVEN USING EXAMPLES SECNARIO OUTLINE,  EXCEL FILE,   DATATABLE  
  *DONE READING DATA FROM EXCEL FILE STORED IN SYSTEM USING SHEET NUMBER ,SHEET NAME 
@@ -17,9 +17,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
  *DONE EXCEPTION HANDLING 
  *USED CONFIG.PROPERTIES FILE,CONFIG READER, EXCEL READER, CONSTANTS, LOG4J, HOOKS,CROSS BROWSER
  *GENERATED EXTENT PDF REPORT , EXTENT SPARK HTML REPORT AND CUCUMBER HTML REPORT    */
-	//@RunWith(Cucumber.class)
-	//@CucumberOptions(
-      @CucumberOptions(
+	@RunWith(Cucumber.class)    //***************  junit
+	@CucumberOptions(           //***************  junit
+    //  @CucumberOptions(            //***************  testng
 			//features = "src/test/resources/features/01_mainPage.feature",
 			//features = "src/test/resources/features/02_registerPage.feature",
 			//features ="src/test/resources/features/03_signIn.feature",
@@ -29,11 +29,11 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 			//features = "src/test/resources/features/06_linkedList.feature",
 			//features = "src/test/resources/features/07_stack.feature",
 			//features = "src/test/resources/features/08_queue.feature",
-			features = "src/test/resources/features/10_graph.feature",
+			//features = "src/test/resources/features/10_graph.feature",
 			//features = "src/test/resources/features/09_tree.feature",
 	       
 		    //tags=  "@homepage,@signIn,@dataStructure,@array ,@graph",
-			//features = "src/test/resources/features/",
+			features = "src/test/resources/features/",
 			glue =  { "StepDefinition", "hooks" },
 			plugin = { "pretty",
              "html:target/cucumber-reports/dsalgo.html",
